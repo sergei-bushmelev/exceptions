45fc2323c2b19f38419d23c718aa4fb5e68b516b
bush_ <bushmelevfz@gmail.com>
2024-11-21 22:14:21 +0300

    Initial commit

---
+++ src/main/java/pro/sky/exceptionsbushmelev/ExceptionsBushmelevApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.exceptionsbushmelev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsBushmelevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsBushmelevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/exceptionsbushmelev/ExceptionsBushmelevApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.exceptionsbushmelev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsBushmelevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
